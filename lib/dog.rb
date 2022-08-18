e2d7454b7f7177645ee0138cddd2ceb19e7e4a81
Brian Wangora <brianwangora@gmail.com>
2022-08-18 17:40:48 +0300

    tests passing

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
+    # setter
+    def name=(name)
+        @name = name
+    end
 
+    def breed=(breed)
+        @breed = breed
+    end
+
+    # getter
+    def name 
+        @name
+    end
+
+    def breed
+        @breed
+    end
 end
+
+hailey = Dog.new
+hailey.name = "Hailey"
+hailey.breed = "Corgi"
+puts hailey.name
+puts hailey.breed
